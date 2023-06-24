53c3ba5d38b50a311a0cc5e493db95f327dc414c
urakawa17 <denys.um@hotmail.com>
2023-06-23 23:35:39 -0300

    Exercicios - Função

--- funcao/exercicio29.js
+++ funcao/exercicio29.js
@@ -2,26 +2,17 @@
 // deste vetor estão no intervalo [10,20] (repare que o intervalo é fechado, ou seja, inclui o 10 e o 20) e quantos
 // deles estão fora do intervalo, escrevendo estas informações.
 
-let numVetor
-let vetor = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
-let countInterval
+function checaIntervalo() {
+  const vetor = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
+  let count = 0
+  for (let i in vetor) {
+    switch (vetor[i] > 10 && vetor[i] < 20) {
+      case true:
+        count++
 
-function checaIntervalo(a, b, numVetor){
-  let minInt = a
-  let maxInt = b
-  console.log("passou22")
-  switch (numVetor) {
-    // case numVetor >= minInt && numVetor <= maxInt:
-    //   countInterval++
-    //   console.log("passou")
-    //   break
-    case 10:
-      console.log("passou33")
-      break
+        break
+    }
   }
+  console.log(count)
 }
-
-for (let i in vetor) {
-  checaIntervalo(10, 20, vetor[i])
-}
-console.log(countInterval)
+checaIntervalo()
--- funcao/testesJS.js
+++ funcao/testesJS.js
@@ -1,9 +1,12 @@
 const vetor = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
-
+let count = 0
 for (let i in vetor) {
-  switch (vetor[i] > 10) {
+  switch (vetor[i] > 10 && vetor[i] < 20) {
     case true:
-      console.log("Passou10")
+      count++
+      
       break
   }
 }
+
+console.log(count)
